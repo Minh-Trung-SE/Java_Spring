e764798f5c7d0422c58ee07ac7efa243a6d2df70
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-04-17 20:21:49 +0700

    add maven project 31

---
+++ Maven_project_demo/src/main/java/com/example/demo/SpringMaven31Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaven31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaven31Application.class, args);
+	}
+
+}
